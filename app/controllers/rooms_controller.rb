83270f1b6948f293fe6a8525e53b119f7e9bcbfb
jendiamond <thejendiamond@gmail.com>
2017-02-24 11:49:36 -0800

    Add rooms#index action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,15 @@
 class RoomsController < ApplicationController
+  before_action :rooms
   def index
-    @rooms = Room.all
+    rooms
   end
 
   def show
+  end
+
+  private
+
+  def rooms
+    @rooms ||= Room.all
   end
 end
